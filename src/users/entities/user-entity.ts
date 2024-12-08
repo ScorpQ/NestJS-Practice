3e9f54022be7ed5128782c9f4abc6d2a17f63ae0
ScorpQ <cansinlale01@gmail.com>
2024-12-08 18:58:00 +0300

    DTO & Entity diffirences

--- src/users/entities/user-entity.ts
+++ src/users/entities/user-entity.ts
@@ -8,3 +8,8 @@ export class User {
 
     // Entities are used to represent data in the database, 
     // while DTOs are used to represent data that is sent to or received from a client.
+
+    // Yani Entity, DB'deki datanın bir sunumu gibi, senin table'ında ... data'ları
+    // bulunmakta. İstersen null olabilir istersen required olabilir.
+    // DTO ise, senin request payload'ında ... data'ları buluanbilir.
+    // Birisi payload/client için birisi DB'deki table için.
